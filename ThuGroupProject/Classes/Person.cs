92addb953d55a2ea05f359ea7fc30ce4ab6a9e1a
ahmed naggar <ahmed_s_naggar@hotmail.com>
2024-08-11 17:02:44 +0300

    Add project files.

---
+++ ThuGroupProject/Classes/Person.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThuGroupProject.Classes
+{
+    public class Person
+    {
+        #region Fields
+        private string _Name;
+        private DateTime _DOB;
+        private string _City;
+        private string _Address;
+        private string _PhoneNumber;
+        private string _IdNumber;
+        private GenderEnum _Gender;
+        #endregion
+
+        #region Properties
+        public string Name { get { return _Name; } set { _Name = value; } }
+        public DateTime DOB { get { return _DOB; } set { _DOB = value; } }
+        public string City { get { return _City; } set { _City = value; } }
+        public string Address { get { return _Address; } set { _Address = value; } }
+        public string PhoneNumber { get { return _PhoneNumber; } set { _PhoneNumber = value; } }
+        public string IdNumber { get { return _IdNumber; } set { _IdNumber = value; } }
+        public GenderEnum Gender { get { return _Gender; } set { _Gender = value; } }
+        #endregion
+
+
+        #region Constructors
+        public Person()
+        {
+            _Name = "New Horaizons";
+            _DOB = new DateTime(2000, 10, 5);
+            _City = "Cairo";
+            _Address = "Nasr City";
+            _PhoneNumber = "115588";
+            _IdNumber = "12345";
+            _Gender = GenderEnum.Male;
+        }
+
+        public Person(string name, DateTime dob, string city, string address, string phoneNumber, string idNumber, GenderEnum gender)
+        {
+           
+            _Name = name;
+            _DOB = dob;
+            _City = city;
+            _Address = address;
+            _PhoneNumber = phoneNumber;
+            _IdNumber = idNumber;
+            _Gender = gender;
+        }
+
+        public Person(Person person)
+        {
+            _Name = person.Name;
+            _DOB = person.DOB;
+            _City= person.City;
+            _Address = person.Address;
+            _PhoneNumber = person.PhoneNumber;
+            _IdNumber = person.IdNumber;
+            _Gender = person.Gender;
+        }
+        #endregion
+
+        #region Methods
+        public string ShowName()
+        {
+            return _Name;
+        }
+
+        public static int Calc()
+        {
+            return 50;
+        }
+        #endregion
+    }
+}
---
+++ ThuGroupProject/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using ThuGroupProject.Classes;
+#region PersonList
+DateTime date = new DateTime(1999, 09, 30);
+List<Person> personsList = new List<Person>();
+for (int i = 1; i <= 10; i++)
+{
+    date = date.AddDays(1);
+    Person person = new Person();
+    person.Name = $"Name {i}";
+    person.DOB = date;
+    person.City = $"City {i}";
+    person.Address = $"Address {i}";
+    person.PhoneNumber = $"PhoneNumber {i}";
+    person.IdNumber = $"IdNumber {i}";
+    personsList.Add(person);
+}
+
+
+foreach (Person person in personsList)
+{
+    string str = $"Person's Name : {person.Name}, Person's BirthDate : {person.DOB.ToShortDateString()}, Person's City = {person.City}, Person's Address : {person.Address}, Person's Phone : {person.PhoneNumber}, Person's Id :{person.IdNumber}";
+    Console.WriteLine(str);
+}
+#endregion
+
+
+
+
+
+
+Console.ReadLine();
